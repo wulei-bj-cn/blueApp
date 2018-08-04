d2e824532b14fa5f162b88da0de3708af9e6f8ec
wulei <wulei@freewheel.tv>
2018-08-04 09:59:30 +0800

    完善用户页面和逻辑，修改User表schema。

--- back-end/blueHouse/src/main/java/com/blueHouse/pojo/User.java
+++ back-end/blueHouse/src/main/java/com/blueHouse/pojo/User.java
@@ -8,6 +8,8 @@ public class User {
     private String name;
     private String password;
     private Integer age;
+    private String phone;
+    private String address;
 
     public int getId() {
         return this.id;
@@ -32,4 +34,17 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    public String getPhone() { return this.phone; }
+
+    public void setPhone(String phone) { this.phone = phone; }
+
+    public String getAddress() { return this.address; }
+
+    public void setAddress(String address) { this.address = address; }
+
+    public int getAge() { return this.age; }
+
+    public void setAge(int age) { this.age = age; }
 }
+
