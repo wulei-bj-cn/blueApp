aec7fa0b523aaf77e1bb14df1deb6996df82e9fc
wulei <wulei@freewheel.tv>
2018-08-04 10:41:36 +0800

    Rename unit test case name.

--- back-end/blueHouse/src/test/java/com/blueHouse/test/TestUserService.java
+++ back-end/blueHouse/src/test/java/com/blueHouse/test/TestUserService.java
@@ -12,7 +12,7 @@ import java.util.List;
 /**
  * Created by wulei on 27/07/2018.
  */
-public class TestService {
+public class TestUserService {
 
     private ApplicationContext applicationContext;
 
