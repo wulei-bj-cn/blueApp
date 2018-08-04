d7b9d6ec85bb59a6aa5f8d06211cf8bce74d7097
wulei <wulei@freewheel.tv>
2018-08-04 10:10:16 +0800

    修改Controller路径指向user页面。

--- back-end/blueHouse/src/main/java/com/blueHouse/controller/UserController.java
+++ back-end/blueHouse/src/main/java/com/blueHouse/controller/UserController.java
@@ -3,7 +3,6 @@ package com.blueHouse.controller;
 /**
  * Created by wulei on 27/07/2018.
  */
-import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.Resource;
 
@@ -12,7 +11,6 @@ import com.blueHouse.service.UserService;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -24,19 +22,10 @@ public class UserController {
 
     @RequestMapping(value = "/getAll", method = RequestMethod.GET)
     public String getAllUsers(ModelMap modelMap) {
-        //ModelAndView mav = new ModelAndView("users");
         List<User> users = userService.findAllUsers();
-        //List<User> users = new ArrayList<>();
-        User user1 = new User();
-        user1.setId(22);
-        user1.setName("Yawei");
-        users.add(user1);
-        for(User user:users){
-            System.out.println(user.getName());
-        }
         modelMap.put("users",users);
-        //mav.addObject("users", users);
-        return "index";
+        modelMap.put("usersCount", users.size());
+        return "users";
     }
 
 }
