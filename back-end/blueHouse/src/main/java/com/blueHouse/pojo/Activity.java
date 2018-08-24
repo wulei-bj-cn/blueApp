435f00532be943a4e8190b422da28dd0c7a40514
lihan3 <lihan3@staff.weibo.com>
2018-08-24 15:19:36 +0800

    add activity

---
+++ back-end/blueHouse/src/main/java/com/blueHouse/pojo/Activity.java
@@ -0,0 +1,54 @@
+package com.blueHouse.pojo;
+
+import java.sql.Timestamp;
+
+/**
+ * Created by lihan on 2018/8/11.
+ */
+public class Activity {
+    private Integer id;
+    private String describe;
+    private String url;
+    private Timestamp start_time;
+    private Timestamp end_time;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescribe() {
+        return describe;
+    }
+
+    public void setDescribe(String describe) {
+        this.describe = describe;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Timestamp getStart_time() {
+        return start_time;
+    }
+
+    public void setStart_time(Timestamp start_time) {
+        this.start_time = start_time;
+    }
+
+    public Timestamp getEnd_time() {
+        return end_time;
+    }
+
+    public void setEnd_time(Timestamp end_time) {
+        this.end_time = end_time;
+    }
+}
