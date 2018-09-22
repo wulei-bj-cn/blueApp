720637d03b2742d3a9f0181d19a1e000c6a194be
wulei <wulei@freewheel.tv>
2018-09-22 16:04:11 +0800

    在Console主页面添加活动页面的入口。

--- back-end/blueHouse/src/main/resources/database/t_activities.sql
+++ back-end/blueHouse/src/main/resources/database/t_activities.sql
@@ -7,3 +7,7 @@ create table if not exists t_activities
 	end_time timestamp
 );
 insert into t_activities values ("act0", "活动1:大大的优惠","www.youhui.com","2018-08-04 15:02:32", "2018-08-04 15:32:24");
+insert into t_activities values ("act1", "活动2:大儿的优惠","www.youhui.com","2018-08-04 15:02:32", "2018-08-04 15:32:24");
+insert into t_activities values ("act2", "活动3:大三的优惠","www.youhui.com","2018-08-04 15:02:32", "2018-08-04 15:32:24");
+insert into t_activities values ("act3", "活动4:大四的优惠","www.youhui.com","2018-08-04 15:02:32", "2018-08-04 15:32:24");
+insert into t_activities values ("act4", "活动5:大五的优惠","www.youhui.com","2018-08-04 15:02:32", "2018-08-04 15:32:24");
