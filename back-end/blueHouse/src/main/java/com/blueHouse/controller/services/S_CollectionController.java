6935c76843ca093c0096e962b898992affae5d53
wulei <wulei@freewheel.tv>
2018-10-13 15:26:02 +0800

    实现小程序接口服务：添加收藏夹。

--- back-end/blueHouse/src/main/java/com/blueHouse/controller/services/S_CollectionController.java
+++ back-end/blueHouse/src/main/java/com/blueHouse/controller/services/S_CollectionController.java
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -81,7 +83,7 @@ public class S_CollectionController {
 
     @RequestMapping(value = "/deleteCollection", method = RequestMethod.GET)
     @ResponseBody
-    public Map<String, Object> getCollectionByCategory(
+    public Map<String, Object> deleteCollection(
             @RequestParam(value = "user_id") String user_id,
             @RequestParam(value = "item_id") String item_id,
             HttpServletRequest req
@@ -99,6 +101,42 @@ public class S_CollectionController {
         collection.setItem_id(item_id);
         try {
             collectionService.deleteCollection(collection);
+        } catch (RuntimeException re) {
+            returnStatus = false;
+        }
+
+        map.put("status", returnStatus);
+        map.put("message", message);
+        map.put("code", httpCode);
+        map.put("error_type", error_type);
+        map.put("data", "");
+
+        return map;
+    }
+
+    @RequestMapping(value = "/insertCollection", method = RequestMethod.GET)
+    @ResponseBody
+    public Map<String, Object> insertCollection(
+            @RequestParam(value = "user_id") String user_id,
+            @RequestParam(value = "item_id") String item_id,
+            @RequestParam(value = "item_class") String item_class,
+            HttpServletRequest req
+    ) {
+
+        Map<String, Object> map = new HashMap<String, Object>();
+        boolean returnStatus = true;
+        String message = "";
+        int httpCode = 200;
+        int error_type = 1;
+
+        CollectionService collectionService = (CollectionService) applicationContext.getBean("collectionService");
+        Collection collection = new Collection();
+        collection.setUser_id(user_id);
+        collection.setItem_id(item_id);
+        collection.setItem_class(item_class);
+        collection.setAdd_time((Timestamp) new Date());
+        try {
+            collectionService.insertCollection(collection);
         } catch (RuntimeException re) {
             returnStatus = false;
         }
