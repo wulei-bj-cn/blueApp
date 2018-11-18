90d3ab239fc65c5d89af32ca14a141e71e954a94
wulei@freewheel.tv <wulei@freewheel.tv>
2018-11-18 18:39:15 +0800

    6.建材里面加一个原价和现价，数据库里面再加一个字段

--- back-end/blueHouse/src/main/resources/database/t_materials.sql
+++ back-end/blueHouse/src/main/resources/database/t_materials.sql
@@ -1,5 +1,5 @@
-create table t_materials (id varchar(64), brand varchar(20), name varchar(256), url varchar(256), category int, price double);
+create table t_materials (id varchar(64), brand varchar(20), name varchar(256), url varchar(256), category int, prePrice double, newPrice DOUBLE);
 
-insert into t_materials values ("mat1", "bra1", "马可波罗瓷砖", "/Users/wulei/Raymond/蓝房子/素材图片/马可波罗瓷砖1.jpeg",1, 28.0);
-insert into t_materials values ("mat2", "bra2", "索菲亚吊顶", "/Users/wulei/Raymond/蓝房子/素材图片/石膏板1.jpeg", 2, 46.5);
+insert into t_materials values ("mat1", "bra1", "马可波罗瓷砖", "/Users/wulei/Raymond/蓝房子/素材图片/马可波罗瓷砖1.jpeg",1, 28.0, 20.0);
+insert into t_materials values ("mat2", "bra2", "索菲亚吊顶", "/Users/wulei/Raymond/蓝房子/素材图片/石膏板1.jpeg", 2, 46.5, 26.5);
 
