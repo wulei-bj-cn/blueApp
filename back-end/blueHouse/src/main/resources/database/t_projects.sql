dd5df81aef71c4b569f5328d96c0d3c91c2f3fa8
wulei@freewheel.tv <wulei@freewheel.tv>
2018-12-29 10:36:37 +0800

    施工项目为元数据，不应该有status，即状态，只有订单和订单项才有状态（status，其他元数据都是没有状态。）

--- back-end/blueHouse/src/main/resources/database/t_projects.sql
+++ back-end/blueHouse/src/main/resources/database/t_projects.sql
@@ -1,6 +1,6 @@
-create table t_projects (id varchar(64), name varchar(32), url varchar(256), enabled int, category varchar(32), ts timestamp, status varchar(32));
+create table t_projects (id varchar(64), name varchar(32), url varchar(256), enabled int, category varchar(32), ts timestamp);
 
-insert into t_projects values ("pro1", "吊顶", "吊顶-a2b19e8f.jpg", 1, "瓦工", "2018-08-04 15:32:24", "进行中");
-insert into t_projects values ("pro2", "改水", "改水-2efa678e.jpg", 1, "水电", "2018-08-04 15:32:24", "进行中");
-insert into t_projects values ("pro3", "改电", "改电-f3278aef.jpg", 1, "水电", "2018-08-04 15:32:24", "进行中");
+insert into t_projects values ("pro1", "吊顶", "吊顶-a2b19e8f.jpg", 1, "瓦工", "2018-08-04 15:32:24");
+insert into t_projects values ("pro2", "改水", "改水-2efa678e.jpg", 1, "水电", "2018-08-04 15:32:24");
+insert into t_projects values ("pro3", "改电", "改电-f3278aef.jpg", 1, "水电", "2018-08-04 15:32:24");
 
