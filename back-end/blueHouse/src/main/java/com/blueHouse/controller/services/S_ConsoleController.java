a24175db0114567da3d3744f6ac90897e04bf01f
wulei@freewheel.tv <wulei@freewheel.tv>
2019-02-09 17:34:22 +0800

    首页中支持page，size，注意是通过配置文件设置的。

--- back-end/blueHouse/src/main/java/com/blueHouse/controller/services/S_ConsoleController.java
+++ back-end/blueHouse/src/main/java/com/blueHouse/controller/services/S_ConsoleController.java
@@ -11,19 +11,20 @@ import com.blueHouse.pojo.browse.T_Solution;
 import com.blueHouse.service.ActivityService;
 import com.blueHouse.service.ArticleService;
 import com.blueHouse.service.SolutionService;
-import com.github.pagehelper.PageHelper;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
 @Controller
 @RequestMapping("/home")
@@ -34,13 +35,34 @@ public class S_ConsoleController {
     SolutionService solutionService = (SolutionService) applicationContext.getBean("solutionService");
     ArticleService articleService = (ArticleService) applicationContext.getBean("articleService");
 
+    private <T> List<T> limitedList(List<T> sourceList, int start_index, int end_index) {
+        List<T> objects = null;
+        if (sourceList != null && start_index < sourceList.size()) {
+            if (end_index < sourceList.size()) {
+                objects = sourceList.subList(start_index, end_index);
+            } else {
+                objects = sourceList.subList(start_index, sourceList.size());
+            }
+        }
+        return objects;
+    }
+
     @RequestMapping(value = "/homePage", method = RequestMethod.GET)
     @ResponseBody
     public Map<String, Object> insertMeasure(
-            @RequestParam(value = "page", required = false) int page,
-            @RequestParam(value = "size", required = false) int size,
             HttpServletRequest req
     ) {
+
+        Properties prop = null;
+        try {
+            prop = PropertiesLoaderUtils.loadAllProperties("conf/blueHouse.properties");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        int home_page =  Integer.parseInt(prop.getProperty("home_page"));
+        int home_size =  Integer.parseInt(prop.getProperty("home_size"));
+        int start_index = home_page * home_size;
+        int end_index = (home_page + 1) * home_size - 1;
 
         Map<String, Object> map = new HashMap<>();
         boolean returnStatus = true;
@@ -51,23 +73,28 @@ public class S_ConsoleController {
         JSONObject jsonObject = new JSONObject();
         JSONObject designsJson = new JSONObject();
 
-        PageHelper.startPage(page, size);
         List<T_Activity> activities = activityService.findAllActivity();
+        List<T_Activity> subActivities = limitedList(activities, start_index, end_index);
 
         List<T_Solution> solutions1 = solutionService.findSolutionByCategory("⽓派中式");
+        List<T_Solution> subSol1 = limitedList(solutions1, start_index, end_index);
         List<T_Solution> solutions2 = solutionService.findSolutionByCategory("奢华欧式");
+        List<T_Solution> subSol2 = limitedList(solutions2, start_index, end_index);
         List<T_Solution> solutions3 = solutionService.findSolutionByCategory("精美⽥园");
+        List<T_Solution> subSol3 = limitedList(solutions3, start_index, end_index);
         List<T_Solution> solutions4 = solutionService.findSolutionByCategory("⼤⽓美式");
+        List<T_Solution> subSol4 = limitedList(solutions4, start_index, end_index);
 
         List<Article> articles = articleService.findAllArticles();
+        List<Article> subArticles = limitedList(articles, start_index, end_index);
 
-        designsJson.put("⽓派中式", solutions1);
-        designsJson.put("奢华欧式", solutions2);
-        designsJson.put("精美⽥园", solutions3);
-        designsJson.put("⼤⽓美式", solutions4);
+        designsJson.put("⽓派中式", subSol1);
+        designsJson.put("奢华欧式", subSol2);
+        designsJson.put("精美⽥园", subSol3);
+        designsJson.put("⼤⽓美式", subSol4);
 
-        jsonObject.put("banner", activities);
-        jsonObject.put("news", articles);
+        jsonObject.put("banner", subActivities);
+        jsonObject.put("news", subArticles);
         jsonObject.put("design", designsJson);
 
         map.put("status", returnStatus);
