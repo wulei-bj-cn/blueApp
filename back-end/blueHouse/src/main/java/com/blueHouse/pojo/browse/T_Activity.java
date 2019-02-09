5ccc707b089657025314ec84ed5cffdbaa79904a
wulei@freewheel.tv <wulei@freewheel.tv>
2019-02-09 15:50:27 +0800

    修改Activity 的schema，适应小程序前端接口需求。

--- back-end/blueHouse/src/main/java/com/blueHouse/pojo/browse/T_Activity.java
+++ back-end/blueHouse/src/main/java/com/blueHouse/pojo/browse/T_Activity.java
@@ -9,6 +9,9 @@ public class T_Activity {
     private String id;
     private String des;
     private String url;
+    private String act_type;
+    private String target_url;
+    private String active_id;
     private Timestamp start_time;
     private Timestamp end_time;
 
@@ -51,4 +54,16 @@ public class T_Activity {
     public void setEnd_time(Timestamp end_time) {
         this.end_time = end_time;
     }
+
+    public String getAct_type() { return act_type; }
+
+    public void setAct_type(String act_type) { this.act_type = act_type; }
+
+    public String getTarget_url() { return this.target_url; }
+
+    public void setTarget_url(String target_url) { this.target_url = target_url; }
+
+    public String getActive_id() { return this.active_id; }
+
+    public void setActive_id(String active_id) { this.active_id = active_id; }
 }
--- back-end/blueHouse/src/main/resources/database/t_activities.sql
+++ back-end/blueHouse/src/main/resources/database/t_activities.sql
@@ -1,13 +1,7 @@
-create table if not exists t_activities
-(
-	id varchar(64) not null,
-	des varchar(256),
-	url varchar(256),
-	start_time timestamp,
-	end_time timestamp
-);
-insert into t_activities values ("act0", "活动1:大大的优惠","www.youhui.com","2018-08-04 15:02:32", "2018-08-04 15:32:24");
-insert into t_activities values ("act1", "活动2:大儿的优惠","www.youhui.com","2018-08-04 15:02:32", "2018-08-04 15:32:24");
-insert into t_activities values ("act2", "活动3:大三的优惠","www.youhui.com","2018-08-04 15:02:32", "2018-08-04 15:32:24");
-insert into t_activities values ("act3", "活动4:大四的优惠","www.youhui.com","2018-08-04 15:02:32", "2018-08-04 15:32:24");
-insert into t_activities values ("act4", "活动5:大五的优惠","www.youhui.com","2018-08-04 15:02:32", "2018-08-04 15:32:24");
+create table if not exists t_activities ( id varchar(64) not null, des varchar(256), url varchar(256), act_type varchar(32), target_url varchar(256), active_id varchar(64), start_time timestamp, end_time timestamp );
+
+insert into t_activities values ("act0", "活动1:大大的优惠","act_pic_1.jpg", "Static", NULL, NULL, "2018-08-04 15:02:32", "2018-08-04 15:32:24");
+insert into t_activities values ("act1", "活动2:大大的优惠","act_pic_2.jpg", "URL", "www.youhui2.com", NULL, "2018-08-04 15:02:32", "2018-08-04 15:32:24");
+insert into t_activities values ("act2", "活动3:大大的优惠","act_pic_3.jpg", "Discounts", NULL, "sol_201xyw802", "2018-08-04 15:02:32", "2018-08-04 15:32:24");
+insert into t_activities values ("act3", "活动4:大大的优惠","act_pic_4.jpg", "Product", NULL, "mat_26wo203923", "2018-08-04 15:02:32", "2018-08-04 15:32:24");
+insert into t_activities values ("act4", "活动5:大大的优惠","act_pic_5.jpg", "URL", "www.youhui5.com", NULL, "2018-08-04 15:02:32", "2018-08-04 15:32:24");
